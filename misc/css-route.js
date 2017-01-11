7bc31e496a25deb8b77b11c15800a2c4895f291f
Cory Loken <cory@me.com>
2017-01-11 14:24:51 -0500

    Create css-route.js

---
+++ misc/css-route.js
@@ -0,0 +1,18 @@
+Ember.Route.reopen({
+  activate: function() {
+    this._super();
+    var cssClass = this.toCssClass();
+    // you probably don't need the application class
+    // to be added to the body
+    if (cssClass !== 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    this._super();
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+});
